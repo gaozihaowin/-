d8be1c429f72a091e9c595f5b3c94785c73d6709
GaoZihao <cmy9551@163.com>
2026-01-29 11:39:52 +0800

    初次创建

---
+++ src/main/java/com/daily/dailychineseculture/DailyChineseCultureApplication.java
@@ -0,0 +1,13 @@
+package com.daily.dailychineseculture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyChineseCultureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyChineseCultureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daily/dailychineseculture/DailyChineseCultureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daily.dailychineseculture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyChineseCultureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
