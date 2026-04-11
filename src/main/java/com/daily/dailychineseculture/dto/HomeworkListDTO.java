e108216cdb603f95776f42f306dd3254561037d1
19712028316 <2055934516@qq.com>
2026-04-11 12:43:17 +0800

    加入了关于作业证书的信息

--- src/main/java/com/daily/dailychineseculture/dto/HomeworkListDTO.java
+++ src/main/java/com/daily/dailychineseculture/dto/HomeworkListDTO.java
@@ -1,60 +1,29 @@
 package com.daily.dailychineseculture.dto;
 
-import lombok.Data;
-
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 /**
  * 作业列表DTO
  */
-@Data
 public class HomeworkListDTO {
-    /**
-     * 作业列表
-     */
     private List<HomeworkItem> list;
+    private int total;
 
-    /**
-     * 总数
-     */
-    private Integer total;
-
-    /**
-     * 作业项
-     */
-    @Data
+    // 内部类：作业项
     public static class HomeworkItem {
-        /**
-         * 作业ID
-         */
         private Integer homeworkId;
-
-        /**
-         * 学生姓名
-         */
         private String name;
-
-        /**
-         * 是否小组优秀作业
-         */
-        private Integer isSmallGroupExcellent; // 0否，1是
-
-
-        /**
-         * 是否大组优秀作业
-         */
-        private Integer isBigGroupExcellent;   // 0否，1是
-
-        /**
-         * 提交时间
-         */
+        private Integer isSmallGroupExcellent;
+        private Integer isBigGroupExcellent;
         private Date submitTime;
+        private String organization;
 
         /**
-         * 所属分组
+         * 证书列表
          */
-        private String organization;
+        private List<CertificateInfo> certificates;
 
         public HomeworkItem() {}
 
@@ -65,6 +34,118 @@ public class HomeworkListDTO {
             this.isBigGroupExcellent = isBigGroupExcellent;
             this.submitTime = submitTime;
             this.organization = organization;
+            this.certificates = new ArrayList<>();
         }
+
+        public HomeworkItem(Integer homeworkId, String name, Integer isSmallGroupExcellent, Integer isBigGroupExcellent, Date submitTime, String organization, List<CertificateInfo> certificates) {
+            this.homeworkId = homeworkId;
+            this.name = name;
+            this.isSmallGroupExcellent = isSmallGroupExcellent;
+            this.isBigGroupExcellent = isBigGroupExcellent;
+            this.submitTime = submitTime;
+            this.organization = organization;
+            this.certificates = certificates != null ? certificates : new ArrayList<>();
+        }
+
+        // 证书信息内部类
+        public static class CertificateInfo {
+            private String type;
+            private Date issueTime;
+
+            public CertificateInfo() {}
+
+            public CertificateInfo(String type, Date issueTime) {
+                this.type = type;
+                this.issueTime = issueTime;
+            }
+
+            public String getType() {
+                return type;
+            }
+
+            public void setType(String type) {
+                this.type = type;
+            }
+
+            public Date getIssueTime() {
+                return issueTime;
+            }
+
+            public void setIssueTime(Date issueTime) {
+                this.issueTime = issueTime;
+            }
+        }
+
+        public Integer getHomeworkId() {
+            return homeworkId;
+        }
+
+        public void setHomeworkId(Integer homeworkId) {
+            this.homeworkId = homeworkId;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getIsSmallGroupExcellent() {
+            return isSmallGroupExcellent;
+        }
+
+        public void setIsSmallGroupExcellent(Integer isSmallGroupExcellent) {
+            this.isSmallGroupExcellent = isSmallGroupExcellent;
+        }
+
+        public Integer getIsBigGroupExcellent() {
+            return isBigGroupExcellent;
+        }
+
+        public void setIsBigGroupExcellent(Integer isBigGroupExcellent) {
+            this.isBigGroupExcellent = isBigGroupExcellent;
+        }
+
+        public Date getSubmitTime() {
+            return submitTime;
+        }
+
+        public void setSubmitTime(Date submitTime) {
+            this.submitTime = submitTime;
+        }
+
+        public String getOrganization() {
+            return organization;
+        }
+
+        public void setOrganization(String organization) {
+            this.organization = organization;
+        }
+
+        public List<CertificateInfo> getCertificates() {
+            return certificates;
+        }
+
+        public void setCertificates(List<CertificateInfo> certificates) {
+            this.certificates = certificates;
+        }
+    }
+
+    public List<HomeworkItem> getList() {
+        return list;
+    }
+
+    public void setList(List<HomeworkItem> list) {
+        this.list = list;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
     }
 }
